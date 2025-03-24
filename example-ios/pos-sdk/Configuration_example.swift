ab4181666c1feef26453204502f8a83f884caf57
bastiaan.verhaar <bastiaan.verhaar@scenius.nl>
2025-03-24 14:57:51 +0100

    wip: start ios example

---
+++ example-ios/pos-sdk/Configuration_example.swift
@@ -0,0 +1,14 @@
+//
+//  Configuration.swift
+//  pos-sdk
+//
+//  Created by Bastiaan Verhaar on 24/03/2025.
+//
+
+//import Foundation
+//
+//class Configuration {
+//    public static let aCode = "A-XXXX-XXXX"
+//    public static let slCode = "SL-XXXX-XXXX"
+//    public static let slSecret = ""
+//}
