da41a1987ff00ea2a398793fcf1315ca73b0ae0d
Bastiaan Verhaar <3987804+bastiaanv@users.noreply.github.com>
2025-04-25 14:29:50 +0200

    test

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "PayNlPOSSdkSwiftWrapper",
+        .binaryTarget(name: "PayNlPOSSdkSwift",
                       url: "https://paysoftpos.blob.core.windows.net/apk/spm-test11.zip",
                       checksum: "32d1c66f1c9e8f9ec434433803b9c85d95831278a0bceca87f17be480e51aa99"
         )
