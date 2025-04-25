5ec9e1b4dc663252ba1b0255d16d60b3b13cc39c
Bastiaan Verhaar <3987804+bastiaanv@users.noreply.github.com>
2025-04-25 16:31:31 +0200

    Swift Package Manager support (#1)

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PayNlPOSSdkSwiftWrapper",
+    platforms: [ .iOS(.v18) ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PayNlPOSSdkSwiftWrapper",
+            targets: ["PayNlPOSSdkSwift"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(name: "PayNlPOSSdkSwift",
+                      url: "https://paysoftpos.blob.core.windows.net/apk/spm-test13.zip",
+                      checksum: "9b6f8f34d8bfbb7cc5a7db731bcd0f4ad7ccbe4a5c5c33ad13ee7ff966d2f876"
+        )
+
+    ]
+)
