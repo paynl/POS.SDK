af28d3b5adfe4d7ab53a27a19cb0bb480d3846e5
SceniusMaCRunnerBiggy <sceniusmacrunnerbiggy@SceniusMaCRunnerBiggys-Mac-Studio.local>
2025-11-13 16:17:48 +0100

    update ios package

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(name: "PayNlPOSSdkSwift",
-                      url: "https://paysoftpos.blob.core.windows.net/apk/pos-sdk-swift-package-manager-0.0.30.zip",
-                      checksum: "057f314aeae0294aa1fac384aeea0160b06ff3811ba9530dd78423ea23ba6e96"
+                      url: "https://paysoftpos.blob.core.windows.net/apk/pos-sdk-swift-package-manager-0.0.31.zip",
+                      checksum: "ebbb9e648eb8b80c4ea979dd9100ba5f8f68162ec5304525d3684afa501b8c43"
         )
 
     ]
